f08563e8a2520adb42474cc0b671e9b1bca212ec
Ayushman Rawat <ayushmanrawat10@gmail.com>
2025-09-09 17:26:35 +0530

    added background image

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
+    <div className='border w-full h-screen sm:px-[15%] sm:py-[15%]'>
       <h1>Home Page</h1>
     </div>
   )
