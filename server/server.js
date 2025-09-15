7c0e3501b1f5b1764b91feccdf58647d0e029b84
Ayushman Rawat <ayushmanrawat10@gmail.com>
2025-09-15 11:32:28 +0530

    created a server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import 'dotenv/config'
+import http from 'http'
+import cors from 'cors'
+
+const app = express()
+const server = http.createServer(app)
+
+app.use(express.json({limit: '4mb'}))
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
+
+app.get('/api/status', (req,res) => {
+    res.send('Server is running')
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
