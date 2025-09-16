019f3a2912f1dd009ca417f680ff5cfa08b2910a
Ayushman Rawat <ayushmanrawat10@gmail.com>
2025-09-16 12:11:20 +0530

    created signup and login controller function

---
+++ server/controller/userController.js
@@ -0,0 +1,93 @@
+import { generateToken } from "../lib/utils.js"
+import User from "../models/user"
+import bcrypt from "bcryptjs"
+
+export const signup = async(req,res) => {
+    const {name, email, password, bio} = req.body
+
+    try{
+        if(!name || !email || !password || !bio){
+            return res.json({
+                success: false,
+                message: 'Missing details'
+            })
+        }
+
+        const user = await User.findOne({email})
+        if(user){
+            return res.json({
+                success: false,
+                message: 'User already exists'
+            })
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = await User.create({
+            name, 
+            email, 
+            password: hashedPassword, 
+            bio
+        })
+
+        const token = generateToken(newUser._id)
+
+        return res.json({
+            success: true,
+            userData: newUser, 
+            token, 
+            message: 'Account created successfully'
+        })
+    }
+    catch(error){
+        return res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export const login = async(req,res) => {
+    const {email, password} = req.body
+
+    try{
+        if(!email || !password){
+            return res.json({
+                success: false,
+                message: 'Missing details'
+            })
+        }
+
+        const user = await User.findOne({email})
+        if(!user){
+            return res.json({
+                success: false,
+                message: 'User does not exist'
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.json({
+                success: false,
+                message: 'Invalid Password'
+            })
+        }
+
+        const token = generateToken(user._id)
+        return res.json({
+            success: true,
+            user,
+            token,
+            message: 'Login Successful'
+        })
+    }
+    catch(error){
+        console.log(error.message)
+        return res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
