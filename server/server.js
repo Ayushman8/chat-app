d93e7783a65d261417683643df9296a3c4f83e2d
Ayushman Rawat <ayushmanrawat10@gmail.com>
2025-09-23 01:30:23 +0530

    created routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import http from 'http'
 import cors from 'cors'
 import { connectDB } from './lib/db.js'
 import userRouter from './routes/userRoutes.js'
+import messageRouter from './routes/messageRoutes.js'
 
 const app = express()
 const server = http.createServer(app)
@@ -20,6 +21,7 @@ app.get('/api/status', (req,res) => {
 })
 
 app.use('/api/auth', userRouter)
+app.use('/api/messages', messageRouter)
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
