f7799dd7d3ace8f013339cf3642c3f553c31fa07
Ayushman Rawat <ayushmanrawat10@gmail.com>
2025-09-24 00:35:24 +0530

    intialized the web socket

--- server/server.js
+++ server/server.js
@@ -5,10 +5,38 @@ import cors from 'cors'
 import { connectDB } from './lib/db.js'
 import userRouter from './routes/userRoutes.js'
 import messageRouter from './routes/messageRoutes.js'
+import { Server } from 'socket.io'
 
 // Create express app and HTTP server
 const app = express()
 const server = http.createServer(app)
+
+// Initialize Socket.io server
+export const io = new Server(server, {
+    cors: {origin: '*'}
+})
+
+// Store Online Users
+export const userSocketMap = {}
+
+// Socket.io connection handler
+io.on('connection', (socket) => {
+    const userId = socket.handshake.query.userId
+    console.log('User Connected', userId)
+
+    if(userId){
+        userSocketMap[userId] = socket.id
+    }
+
+    // Emit online users to all connected clients
+    io.emit('getOnlineUsers', Object.keys(userSocketMap))
+    
+    socket.on('disconnect', () => {
+        console.log('User Disconnected', userId)
+        delete userSocketMap[userId]
+        io.emit('getOnlineUsers', Object.keys(userSocketMap))
+    })
+})
 
 // Middleware setup
 app.use(express.json({limit: '4mb'}))
