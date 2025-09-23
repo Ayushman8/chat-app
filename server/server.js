509405b42537a4c4643a42bc461f88864114a0ad
Ayushman Rawat <ayushmanrawat10@gmail.com>
2025-09-23 22:42:08 +0530

    basic changes

--- server/server.js
+++ server/server.js
@@ -6,16 +6,15 @@ import { connectDB } from './lib/db.js'
 import userRouter from './routes/userRoutes.js'
 import messageRouter from './routes/messageRoutes.js'
 
+// Create express app and HTTP server
 const app = express()
 const server = http.createServer(app)
 
+// Middleware setup
 app.use(express.json({limit: '4mb'}))
 app.use(cors())
 
-await connectDB()
-
-const PORT = process.env.PORT || 5000
-
+// Routes Setup
 app.get('/api/status', (req,res) => {
     res.send('Server is running')
 })
@@ -23,6 +22,10 @@ app.get('/api/status', (req,res) => {
 app.use('/api/auth', userRouter)
 app.use('/api/messages', messageRouter)
 
+// Connect to Database
+await connectDB()
+
+const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
